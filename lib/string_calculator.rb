396bbfa441af3698b1844ed6fb8f429a60ae5b63
Himalee <himalee.tailor@gmail.com>
2018-04-24 15:29:29 +0100

    First commit

---
+++ lib/string_calculator.rb
@@ -0,0 +1,12 @@
+# lib/string_calculator.rb
+class StringCalculator
+
+  def self.add(string)
+    if string == ""
+      0
+    else
+      string.to_i
+    end
+  end
+
+end
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,30 @@
+# spec/string_calculator_spec.rb
+require "string_calculator"
+
+describe StringCalculator do
+
+  describe ".add" do
+
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eql(0)
+      end
+    end
+    context "given string with one number" do
+      it "returns one for one" do
+        expect(StringCalculator.add("1")).to eql(1)
+    end
+      end
+      it "returns two for two" do
+        expect(StringCalculator.add("2")).to eql(2)
+      end
+    context "two numbers" do
+      context "given '2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eql(6)
+        end
+      end
+    end
+
+  end
+end
